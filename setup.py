b0aa69dc0ca91ec6c3ac0962c78b63cc28a9c438
Eric Dill <edill@bnl.gov>
2014-11-19 12:20:41 -0500

    MNT: Improved setuptools in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from __future__ import (absolute_import, division, print_function)
+
+
+import sys
+import warnings
+try:
+    from setuptools import setup
+except ImportError:
+    try:
+        from setuptools.core import setup
+    except ImportError:
+        from distutils.core import setup
+
+from distutils.core import setup, Extension
+
 
 setup(name='lupit',
       version='0.1',
